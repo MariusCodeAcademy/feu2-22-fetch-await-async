bde17299b15df1cf4608085f8a037f1a938d71db
Marius <marius@example.com>
2022-11-10 15:52:18 +0200

    app works

--- js/users-app.js
+++ js/users-app.js
@@ -3,6 +3,9 @@ console.log('users-app.js');
 
 const url = 'https://reqres.in/api/users?page=1';
 const uListEl = document.getElementById('usersList');
+const currentUserEl = document.getElementById('currentUser');
+
+let dataArr = [];
 
 function getData(from) {
   return fetch(from)
@@ -22,12 +25,12 @@ function makeCardLi(obj) {
   const liEl = document.createElement('li');
   liEl.className = 'list-el';
   liEl.textContent = `${obj.first_name} ${obj.last_name}`;
-  liEl.addEventListener('click', () => showUser());
+  liEl.addEventListener('click', () => showUser(obj.id));
   return liEl;
 }
 
 async function mainInit() {
-  const dataArr = await getData(url);
+  dataArr = await getData(url);
   makeList(dataArr, makeCardLi);
 }
 
@@ -35,5 +38,29 @@ mainInit();
 
 function showUser(userId) {
   //
-  console.log('show user');
+  console.log('show user', userId);
+  // surasti el su id userId
+  const found = dataArr.find((uObj) => uObj.id === userId);
+  if (!found) return console.warn('not found');
+  console.log('found ===', found);
+  // pagaminti el su makeCard()
+  const htmlCardEl = makeCard(found);
+  // isvalyti konteineri pries talpinant
+  currentUserEl.innerHTML = '';
+  // patalipinti el currentUserEl
+  currentUserEl.append(htmlCardEl);
+}
+
+function makeCard(obj) {
+  const divEl = document.createElement('div');
+  divEl.className = 'card card--user';
+  const imgEl = document.createElement('img');
+  imgEl.src = obj.avatar;
+  imgEl.alt = obj.email;
+  const h3El = document.createElement('h3');
+  h3El.textContent = `${obj.first_name} ${obj.last_name}`;
+  const pEl = document.createElement('p');
+  pEl.textContent = `${obj.email} (id:${obj.id})`;
+  divEl.append(imgEl, h3El, pEl);
+  return divEl;
 }
