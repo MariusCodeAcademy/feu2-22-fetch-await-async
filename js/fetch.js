dd07c1e5fa2593a56b2952ebf806fc22220d62f8
Marius <marius@example.com>
2022-11-10 08:57:34 +0200

    fetch footer

--- js/fetch.js
+++ js/fetch.js
@@ -3,7 +3,31 @@ console.log('fetch.js');
 
 //parsisiusti user.json
 
-fetch('data/user.json')
-  .then((response) => response.json())
-  .then((data) => console.log('data ===', data))
-  .catch((err) => console.warn('klaida', err));
+function getLocalUser() {
+  fetch('data/user.json')
+    .then((response) => response.json())
+    .then((data) => console.log('data ===', data))
+    .catch((err) => console.warn('klaida', err));
+}
+// getLocalUser();
+
+function getLocalText() {
+  fetch('data/colors.txt')
+    .then((response) => response.text())
+    .then((data) => {
+      console.log('data ===', data);
+      const arr = data.split(', ');
+      console.log('arr ===', arr);
+    })
+    .catch((err) => console.warn('klaida', err));
+}
+
+getLocalText();
+
+const initHtml = () =>
+  fetch('data/footer.html')
+    .then((response) => response.text())
+    .then((data) => document.body.insertAdjacentHTML('beforeend', data))
+    .catch((err) => console.warn('klaida', err));
+
+initHtml();
